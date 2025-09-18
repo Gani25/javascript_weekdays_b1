7b3496113c943e1a4ae5c6ec102fbae987fcd465
Gani25 <memonabdulgani3d@gmail.com>
2025-09-18 19:28:32 +0530

    Array more methods

--- day4/arr2.js
+++ day4/arr2.js
@@ -4,6 +4,12 @@
 2. pop -> Delete from end and return the deleted element
 3. unshift -> add to start
 4. shift -> Delete from start and return the deleted element
+5. indexOf -> Return indes if element is present or -1 if not present
+6. includes -> returns true if element is present or false
+7. concat -> Join 2 arrays
+8. reverse -> 
+9. slice
+10. splice
 */
 
 let names = ["Rohit", "Shruti", "Shoeb", "Lalit"];
