d68a64630cd1a5d0d533680d9d12943c1a0ea065
Gani25 <memonabdulgani3d@gmail.com>
2025-09-30 18:59:29 +0530

    Function

---
+++ day9/fn3.js
@@ -0,0 +1,20 @@
+// Write a function which accepts a number and create a random between 1 to number
+// number - 10 [1,10]
+
+function randomNumber(number) {
+  randomNum = Math.floor(Math.random() * number) + 1;
+  console.log("Number = ", number, " Random = ", randomNum);
+}
+randomNumber(6);
+randomNumber(6);
+randomNumber(6);
+randomNumber(6);
+randomNumber(6);
+randomNumber(6);
+randomNumber(6);
+randomNumber(6);
+randomNumber(6);
+randomNumber(6);
+
+// Create a function -> takes 3 numebr display sum and average
+// Create a function -> Takes a number and x iteration display table of number till x iteration
